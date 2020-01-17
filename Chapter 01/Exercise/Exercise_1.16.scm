ed8a8472a3d3cfb967fcfaaf1b6a5986c6df8a23
jklf5-pc <691805562@qq.com>
2020-01-17 17:22:34 +0800

    练习1.16 按照迭代方式的求幂计算过程

---
+++ Chapter 01/Exercise/Exercise_1.16.scm
@@ -0,0 +1,12 @@
+(define (square x) (* x x))
+
+(define (even? n)
+    (= (remainder n 2) 0))
+; 利用所给关系，并且使用一个不变量记录中间结果
+(define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* a b)))))
+
+(define (fast-expt b n)
+    (fast-expt-iter b n 1))
