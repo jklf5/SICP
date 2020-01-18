80736946940d0157eb4a5089886c8b1ec8024ea7
jklf5 <691805562@qq.com>
2020-01-18 20:26:16 +0800

    :hammer: 练习1.3 求解三个数中较大两数的平方和

--- Chapter01/Exercise/Exercise-1.3.scm
+++ Chapter01/Exercise/Exercise-1.3.scm
@@ -1,12 +1,13 @@
 ; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers
-; 给出三个数，计算其中两个较大数的和
+; 给出三个数，计算其中两个较大数的平方和
 ; 定义一个求两个数中较小值的过程
 (define (min a b)
     (if (< a b)
         a
         b))
-; 定义求解三数中较大数和的过程
+(define (square x)
+    (* x x))
+; 定义求解三数中较大数平方和的过程
 (define (sum a b c)
-    (- (+ a b c)
-        (min (min a b) c)
-    ))
+    (- (+ (square a) (square b) (square c))
+        (square (min (min a b) c))))
