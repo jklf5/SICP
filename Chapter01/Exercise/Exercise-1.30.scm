5c2660107f0a267a9d9d619b4ef09a2587ef3634
jklf5 <691805562@qq.com>
2020-01-23 13:40:00 +0800

    :tada: 练习1.30 重写sum为迭代计算过程

---
+++ Chapter01/Exercise/Exercise-1.30.scm
@@ -0,0 +1,7 @@
+; 重写sum过程，改为迭代计算过程
+(define (sum term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter (next a) (+ result (term a)))))
+    (iter a 0))
